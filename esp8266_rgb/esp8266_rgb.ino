5b33c56e7d9751a454f8e2adfb823b265dcf477b
unknown <2801079570@qq.com>
2019-02-24 06:48:49 +0800

    增加了EEPROM功能

--- esp8266_rgb/esp8266_rgb.ino
+++ esp8266_rgb/esp8266_rgb.ino
@@ -3,33 +3,41 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
+#include "EEPROM.h"
 
 #define R D0
 #define G D1
 #define B D2
 
-IPAddress apIP(192, 168, 1, 114);
+IPAddress apIP(192, 168, 1, 1);
 ESP8266WebServer webServer(80);
 String httpBuff = "";
-u8 delayTime;
-//Wifi配置
-const char *SSID = "ESP8266WiFi";
+u8 delayTime = 2;
+
+u8 color0Num,color1Num;
+
+u8 colorGroup0[21][3];
+u8 colorGroup1[21][3];
 
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("");
+
   pinMode(R, OUTPUT);
   pinMode(G, OUTPUT);
   pinMode(B, OUTPUT);
-  delayTime = 2;
+  // eeprom初始化
+  EEPROM.begin(256);
+  // rgb参数获取
+  getRGBValues();
   //WiFi设置
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP(SSID);
+  WiFi.softAP(eepromReadStr(0x00), eepromReadStr(0x10), 10, 0, 2);
+
   Serial.print("[Debug]->软AP WiFi已启动, SSID: ");
-  Serial.print(SSID);
+  Serial.print(WiFi.softAPSSID());
   Serial.print(", IP地址: ");
   Serial.println(WiFi.softAPIP());
   //Web服务器设置
@@ -45,7 +53,7 @@ void setup()
   Serial.println("[Debug]->SPIFFS已加载完毕");
 }
 
-/**
+/* *
  * 循环
  * */
 void loop()
@@ -53,7 +61,7 @@ void loop()
   webServer.handleClient();
 }
 
-/**
+/* *
  * Web服务器事件
  * */
 void handleRoot()
@@ -146,5 +154,72 @@ void dazzling()
   {
     analogWrite(B, map(i - 1, 0, 63, 1023, 0));
     delay(delayTime);
+  }
+}
+/* *
+ * 读取一串数据
+ * */
+u8 *eepromRead(u16 addr, u16 num) {
+  u8 *p;
+  for(u16 i = 0; i < num; i++) {
+    p[i] = EEPROM.read(addr+i);
+  }
+  return p;
+}
+/* *
+ * 读取字符串
+ * */
+String eepromReadStr(u16 addr) {
+  String str;
+  char buff;
+  for(u16 i = 0; i < 16; i++) {
+    buff = EEPROM.read(addr+i);
+    if (buff =='\0')
+      return str;
+    str += buff;
+  }
+  return "ERROR";
+}
+/* *
+ * 写入一串数据
+ * */
+void eepromWrite(u16 addr, u8 *p, u16 num) {
+  for(u16 i = 0; i < num; i++) {
+    EEPROM.write(addr+i,p[i]);
+  }
+  EEPROM.commit();
+}
+/* *
+ * 写入字符串
+ * */
+void eepromWriteStr(u16 addr, String str) {
+  u16 numToWrite = str.length()+1;
+  for(u16 i = 0; i < numToWrite; i++) {
+    EEPROM.write(addr+i,str[i]);
+  }
+  EEPROM.commit();
+}
+/* *
+ * 读取两组RGB数据
+ * */
+void getRGBValues() {
+  u8 buff[64];
+  for(u8 i = 0; i < 64; i++) {
+    buff[0] = EEPROM.read(0x20+i);
+  }
+  color0Num = buff[63];
+  for(u8 i = 0; i < color0Num; i++) {
+    colorGroup0[i][0] = buff[i*3];
+    colorGroup0[i][1] = buff[i*3+1];
+    colorGroup0[i][2] = buff[i*3+2];
+  }
+  for(u8 i = 0; i < 64; i++) {
+    buff[0] = EEPROM.read(0x60+i);
+  }
+  color0Num = buff[63];
+  for(u8 i = 0; i < color0Num; i++) {
+    colorGroup0[i][0] = buff[i*3];
+    colorGroup0[i][1] = buff[i*3+1];
+    colorGroup0[i][2] = buff[i*3+2];
   }
 }
