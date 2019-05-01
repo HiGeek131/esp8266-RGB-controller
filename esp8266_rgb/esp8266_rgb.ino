db657f5662e6aae2a5f29c1b185d4c69bd11a89f
unknown <2801079570@qq.com>
2019-05-02 02:17:01 +0800

    完善RGB功能

--- esp8266_rgb/esp8266_rgb.ino
+++ esp8266_rgb/esp8266_rgb.ino
@@ -1,56 +1,82 @@
 #include "FS.h"
-#include "esp8266_io.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
 #include "EEPROM.h"
 
-#define R D0
-#define G D1
-#define B D2
+#define R 13
+#define G 14
+#define B 15
+#define KEY 4
 
 IPAddress apIP(192, 168, 1, 1);
 ESP8266WebServer webServer(80);
+String SSID,passwd;
 String httpBuff = "";
-u8 delayTime = 2;
 
-u8 color0Num,color1Num;
+// 每个模式下的颜色个数
+u8 mode0Num, mode1Num;
 
-u8 colorGroup0[21][3];
-u8 colorGroup1[21][3];
+// 每个模式下的颜色缓存
+u8 colorGroup0[21][3];  // 模式一可以储存21种颜色
+u8 colorGroup1[25][5];  // 模式二可以储存25种颜色，前三位为颜色信息，后两位为延时信息
+
+// 每个模式下必须的全局变量
+u16 mode0Speed; // 爆闪速度，此为半周期延时，单位为毫秒。
+u8 mode0BlinkFlag;  // 爆闪标志位
+u8 mode2Speed = 2;  // 滚动速度，此为滚动速度。
+u8 mode0ColorFlag, mode1ColorFlag; // 此为当前模式下颜色所处于的位数。
+
+// 模式标志位
+u8 modeFlag = 0;
+
+// 按键辅助标志位
+u32 timeKey;   // 双击间隔时间辅助位
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("");
+  Serial.println("123");
 
   pinMode(R, OUTPUT);
   pinMode(G, OUTPUT);
   pinMode(B, OUTPUT);
+  pinMode(KEY, INPUT);
+  Serial.println("IO");
   // eeprom初始化
   EEPROM.begin(256);
-  // rgb参数获取
+  Serial.println("EEPROM");
   getRGBValues();
+  Serial.println("READ");
+  SSID = eepromReadStr(0x00);
+  Serial.println("READ");
+  passwd = eepromReadStr(0x10);
+  Serial.println("READ");
   //WiFi设置
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP(eepromReadStr(0x00), eepromReadStr(0x10), 10, 0, 2);
+  WiFi.softAP(SSID, passwd, 6, 0, 2);
+  Serial.println("WIFI");
 
   Serial.print("[Debug]->软AP WiFi已启动, SSID: ");
   Serial.print(WiFi.softAPSSID());
   Serial.print(", IP地址: ");
   Serial.println(WiFi.softAPIP());
+  //SPIFFS设置
+  SPIFFS.begin();
+  Serial.println("[Debug]->SPIFFS已加载完毕");
   //Web服务器设置
   webServer.on("/", handleRoot);         //根文件
   webServer.on("/update", handleUpdate); //更新
+  Serial.println("server");
   webServer.begin();
   Serial.print("[Debug]->Web服务器已启动, IP: ");
   Serial.print(WiFi.softAPIP());
   Serial.print(", 端口:");
   Serial.println(80);
-  //SPIFFS设置
-  SPIFFS.begin();
-  Serial.println("[Debug]->SPIFFS已加载完毕");
+  setColor(0, 0, 0);
+
+  attachInterrupt(KEY, keyIntt, FALLING);
 }
 
 /* *
@@ -59,6 +85,23 @@ void setup()
 void loop()
 {
   webServer.handleClient();
+  switch (modeFlag)
+  {
+    case 0:
+      mode0();
+      break;
+
+    case 1:
+      mode1();
+      break;
+
+    case 2:
+      mode2();
+      break;
+  
+    default:
+      break;
+  }
 }
 
 /* *
@@ -84,7 +127,7 @@ void handleUpdate()
   DynamicJsonDocument jsonBuffer(capacity);
   DeserializationError error = deserializeJson(jsonBuffer, str);
   if (error) {
-    Serial.print(F("deserializeJson() failed: "));
+    Serial.print("deserializeJson() failed: ");
     Serial.println(error.c_str());
     return;
   }
@@ -101,59 +144,135 @@ void handleUpdate()
     webServer.send(200, "application/json", str);
   }
 }
+/**
+ * r: 8bit 红色
+ * g: 8bit 绿色
+ * b: 8bit 蓝色
+ */
+void keyIntt() {
+  delay(20);
+  if (digitalRead(KEY) == LOW) {
+    switch (modeFlag) {
+      case 0:
+        if (millis() - timeKey < 1000) {
+          mode0BlinkFlag ? mode0BlinkFlag = 0 : mode0BlinkFlag = 1;
+        }
+        timeKey = millis();
+        setColor(0, 0, 0);
+        while(digitalRead(KEY) == LOW) {
+          u16 holdTime;
+          delay(10);
+          holdTime ++;
+          if (holdTime = 100) {
+            modeFlag ++;
+          }
+        }
+        mode0ColorFlag > mode0Num ? mode0ColorFlag = 0 : mode0ColorFlag ++ ;
+        break;
+
+        case 1:
+        setColor(0, 0, 0);
+        while(digitalRead(KEY) == LOW) {
+          u16 holdTime;
+          delay(10);
+          holdTime ++;
+          if (holdTime = 100) {
+            modeFlag ++;
+          }
+        }
+        break;
+
+        case 2:
+        setColor(0, 0, 0);
+        while(digitalRead(KEY) == LOW) {
+          u16 holdTime;
+          delay(10);
+          holdTime ++;
+          if (holdTime = 100) {
+            modeFlag ++;
+          }
+        }
+        mode2Speed == 2 ? mode2Speed = 5 : mode2Speed = 2;
+        break;
+    
+      default:
+        break;
+    }
+  }
+}
 
 /**
  * r: 8bit 红色
  * g: 8bit 绿色
  * b: 8bit 蓝色
  */
-void setColor(u8 r, u8 g, u8 b)
-{
-//测试
+void setColor(u8 r, u8 g, u8 b) {
   analogWrite(R, map(r, 0, 255, 0, 1023));
   analogWrite(G, map(g, 0, 255, 0, 1023));
   analogWrite(B, map(b, 0, 255, 0, 1023));
-
-//  成品使用  
-//  analogWrite(R, map(r, 0, 255, 1023, 0));
-//  analogWrite(G, map(g, 0, 255, 1023, 0));
-//  analogWrite(B, map(b, 0, 255, 1023, 0));
 }
 
-void dazzling()
-{
-  analogWrite(R, 1023);
-  analogWrite(G, 0);
-  analogWrite(B, 0);
+void mode0() {
+  setColor(colorGroup0[mode1ColorFlag][0], colorGroup0[mode1ColorFlag][1], colorGroup0[mode1ColorFlag][2]);
+  if (mode0BlinkFlag) {
+    setColor(0, 0, 0);
+    delay(mode0Speed);
+    setColor(colorGroup0[mode1ColorFlag][0], colorGroup0[mode1ColorFlag][1], colorGroup0[mode1ColorFlag][2]);
+    delay(mode0Speed);
+  }
+  
+}
+
+void mode1() {
+  setColor(colorGroup1[mode1ColorFlag][0], colorGroup1[mode1ColorFlag][1], colorGroup1[mode1ColorFlag][2]);
+  u16 delayTime = (u16) colorGroup1[mode1ColorFlag][3] << 8;
+  delayTime += (u16) colorGroup1[mode1ColorFlag][4];
+  delay(delayTime);
+  mode1ColorFlag > mode1Num ? mode1ColorFlag = 0 : mode1ColorFlag ++;
+}
+
+void mode2() {
+  // u8 sinR, sinG, sinB;
+  // sinR = map(sin((double) (mode2ColorFlag / 100) * 3.14) * 100, -100, 100, 0, 255);
+  // sinR = map(sin((double) (mode2ColorFlag / 100) * 3.14 + 3.14/3) * 100, -100, 100, 0, 255);
+  // sinR = map(sin((double) (mode2ColorFlag / 100) * 3.14 + 3.14*2/3) * 100, -100, 100, 0, 255);
+
+  // setColor(sinR, sinG, sinB);
+
+  // delay(40);
+
+  // mode2ColorFlag > 100 ? mode2ColorFlag = 0 : mode2ColorFlag += mode2Speed;
+  
+  
   for (int i = 0; i < 64; i++)
   {
     analogWrite(G, map(i, 0, 63, 1023, 0));
-    delay(delayTime);
+    delay(mode2Speed);
   }
   for (int i = 64; i > 0; i--)
   {
     analogWrite(R, map(i - 1, 0, 63, 1023, 0));
-    delay(delayTime);
+    delay(mode2Speed);
   }
   for (int i = 0; i < 64; i++)
   {
     analogWrite(B, map(i, 0, 63, 1023, 0));
-    delay(delayTime);
+    delay(mode2Speed);
   }
   for (int i = 64; i > 0; i--)
   {
     analogWrite(G, map(i - 1, 0, 63, 1023, 0));
-    delay(delayTime);
+    delay(mode2Speed);
   }
   for (int i = 0; i < 64; i++)
   {
     analogWrite(R, map(i, 0, 63, 1023, 0));
-    delay(delayTime);
+    delay(mode2Speed);
   }
   for (int i = 64; i > 0; i--)
   {
     analogWrite(B, map(i - 1, 0, 63, 1023, 0));
-    delay(delayTime);
+    delay(mode2Speed);
   }
 }
 /* *
@@ -174,20 +293,12 @@ String eepromReadStr(u16 addr) {
   char buff;
   for(u16 i = 0; i < 16; i++) {
     buff = EEPROM.read(addr+i);
-    if (buff =='\0')
+    if (buff =='\0') {
       return str;
+    }
     str += buff;
   }
   return "ERROR";
-}
-/* *
- * 写入一串数据
- * */
-void eepromWrite(u16 addr, u8 *p, u16 num) {
-  for(u16 i = 0; i < num; i++) {
-    EEPROM.write(addr+i,p[i]);
-  }
-  EEPROM.commit();
 }
 /* *
  * 写入字符串
@@ -205,7 +316,7 @@ void eepromWriteStr(u16 addr, String str) {
 void getRGBValues() {
   u8 buff[64];
   for(u8 i = 0; i < 64; i++) {
-    buff[0] = EEPROM.read(0x20+i);
+    buff[i] = EEPROM.read(0x20+i);
   }
   color0Num = buff[63];
   for(u8 i = 0; i < color0Num; i++) {
@@ -213,10 +324,10 @@ void getRGBValues() {
     colorGroup0[i][1] = buff[i*3+1];
     colorGroup0[i][2] = buff[i*3+2];
   }
-  for(u8 i = 0; i < 64; i++) {
-    buff[0] = EEPROM.read(0x60+i);
+  for(u8 i = 0; i < 128; i++) {
+    buff[i] = EEPROM.read(0x60+i);
   }
-  color1Num = buff[63];
+  color1Num = buff[127];
   for(u8 i = 0; i < color0Num; i++) {
     colorGroup1[i][0] = buff[i*3];
     colorGroup1[i][1] = buff[i*3+1];
